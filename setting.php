8aa0f3acb859c3ebaba0361f45c7a4fb45ae8173
kholizaja <56310771+kholizaja@users.noreply.github.com>
2019-11-14 18:02:24 +0000

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,22 @@
+<?php
+/* setting bot */
+$ads=$ijo."    [ official chanel cah ndeso ]".$t.$red."\t >> subcribe chanel << ".$t.$pth."[ ".$ijo."AW 2000".$pth." ]=[".$kn." Adi Bordir ".$pt>
+
+$donasi=false;
+
+$warn=$red."[!] warning !!!
+".$kn."[-] ini adalah program ilegal
+".$kn."[-] resiko sepenuhnya di tanggung pengguna".$t;
+
+$key="adidoank";
+$link_yogo="https://youtu.be/hLkAqZM3QHc";
+
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$ijo kholiz$putih || code invite:$ijo F26lovOBUQ$putih
+chanel :$kuning cahndeso$putih || code invite:$ijo F3NW34owm4".$t;
+$msg_baca= "[•] official chanel cahndeso".$t."[•] update skrip fix error 14/02/2019";
+$stat_baca=true;
+?>
